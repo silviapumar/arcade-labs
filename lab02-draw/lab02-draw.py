76096b08cf0adac03148aba923277c695897dbc2
spumarf <spumarf@gmail.com>
2023-02-16 16:15:21 +0100

    Segundo commit del LAB_2.

--- lab02-draw/lab02-draw.py
+++ lab02-draw/lab02-draw.py
@@ -9,12 +9,15 @@ arcade.set_background_color(arcade.color.AMARANTH)
 # Get ready to draw.
 arcade.start_render()
 
+# The idea is to draw a watermelon.
 # Draw the "crust" of the watermelon.
-# (left, right, top, bottom)
 arcade.draw_lrtb_rectangle_filled(0, 800, 160, 100, arcade.color.ALMOND)
 arcade.draw_lrtb_rectangle_filled(0, 800, 100, 0, arcade.color.BANGLADESH_GREEN)
 
 # Draw the stripes of the "crust".
+arcade.draw_lrtb_rectangle_filled(10, 55, 100, 0, arcade.color.DARK_JUNGLE_GREEN)
+arcade.draw_lrtb_rectangle_filled(15, 55, 100, 0, arcade.color.DARK_JUNGLE_GREEN)
+arcade.draw_lrtb_rectangle_filled(5, 35, 100, 30, arcade.color.DARK_JUNGLE_GREEN)
 
 
 # Finish the drawing.
