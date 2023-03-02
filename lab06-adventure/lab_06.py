04a07452dca57a6957a6c575d2d6ae33e5df5902
spumarf <spumarf@gmail.com>
2023-03-02 16:51:07 +0100

    Tercer commit de la sesión de LAB-4

--- lab06-adventure/lab_06.py
+++ lab06-adventure/lab_06.py
@@ -10,17 +10,24 @@ class Room:
 
 def main():
     room_list = []
-    room: Room = Room("There's a 'creepy-looking' hospital down the road, next to a river that almost has no water running down it.", 1, None, None, None )
+    room: Room = Room("There's a 'creepy-looking' hospital down the road, next to a river that almost has no water "
+                      "running down it.", 1, None, None, None)
     room_list.append(room)
-    room = Room("You're in the entrance of the abandoned hospital, it's much more creepier inside! There's a key on floor next to some broken glass.", 5, 0, 2, None)
+    room = Room("You're in the entrance of the abandoned hospital, it's much more creepier inside! There's a key on "
+                "floor next to some broken glass.", 5, 0, 2, None)
     room_list.append(room)
-    room = Room("This looks like a waiting room, but the chairs are barely holding it's form and you can see some rats here and there. Also, there's a door at the north of the room below a sign that reads 'Surgery Room'.", 3, None, None, 1)
+    room = Room("This looks like a waiting room, but the chairs are barely holding it's form and you can see some "
+                "rats here and there. Also, there's a door at the north of the room below a sign that reads 'Surgery "
+                "Room'.", 3, None, None, 1)
     room_list.append(room)
-    room = Room("After opening the door you're shocked by the smell of death. There's a body or what remains of it in the center of the room. It smells so bad in here!", None, 2, 4, 5)
+    room = Room("After opening the door you're shocked by the smell of death. There's a body or what remains of it in "
+                "the center of the room. It smells so bad in here!", None, 2, 4, 5)
     room_list.append(room)
-    room = Room("You arrived at a bathroom, you'll pretty lucky if anything works in here. There's a flashlight near a corner.", None, None, None, 3)
+    room = Room("You arrived at a bathroom, you'll pretty lucky if anything works in here. There's a flashlight near "
+                "a corner.", None, None, None, 3)
     room_list.append(room)
-    room = Room("It's pitch black in the room. You can't see a thing but you hear a cracking sound coming from this room.", None, 1, 3, None)
+    room = Room("It's pitch black in the room. You can't see a thing but you hear a cracking sound coming from this "
+                "room.", None, 1, 3, None)
     room_list.append(room)
     print(room_list[0])
     current_room = 0
