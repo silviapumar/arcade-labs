561f0db1a9c07708fedb01b6a275b5b218d14838
spumarf <spumarf@gmail.com>
2023-03-09 16:47:21 +0100

    Tercer commit de la sesión de LAB-5

--- lab07-control/lab07-control.py
+++ lab07-control/lab07-control.py
@@ -8,17 +8,12 @@ SCREEN_HEIGHT = 600
 
 
 def draw_sand():
-    """Draw the sand at the bottom of the ocean."""
     arcade.draw_lrtb_rectangle_filled(0, 800, 120, 0, arcade.color.CAMEL)
 
 
 def draw_fish(x, y):
-    """Draw multiple fish in the ocean."""
-    # Body.
     arcade.draw_ellipse_filled(300 + x, 200 + y, 90, 50, arcade.color.OLD_SILVER)
-    # Tail.
     arcade.draw_triangle_filled(290 + x, 200 + y, 240 + x, 220 + y, 240 + x, 180 + y, arcade.color.OLD_SILVER)
-    # Eye.
     arcade.draw_circle_filled(325 + x, 205 + y, 5, arcade.color.TAUPE)
 
 
@@ -35,8 +30,12 @@ class SeaStar:
         self.color = color
 
     def draw(self):
-        arcade.draw_triangle_filled(150, 350, 100, 275, 200, 275, arcade.color.ORANGE)
-        # arcade.draw_circle_filled(self.position_x, self.position_y, self.radius, self.color)
+        # arcade.draw_triangle_filled(250, 310, 258, 258, 242, 258, self.color)
+        # arcade.draw_triangle_filled(220, 200, 255, 250, 240, 255, self.color)
+        # arcade.draw_triangle_filled(280, 200, 258, 245, 242, 245, self.color)
+        # arcade.draw_triangle_filled(250, 190, 258, 245, 242, 245, self.color)
+        # arcade.draw_triangle_filled(250, 190, 258, 245, 242, 245, self.color)
+        arcade.draw_circle_filled(self.position_x, self.position_y, self.radius, self.color)
 
 
 class MyGame(arcade.Window):
@@ -50,7 +49,7 @@ class MyGame(arcade.Window):
 
         arcade.set_background_color(arcade.color.BLUE)
 
-        self.ball = SeaStar(50, 50, 20, arcade.color.YELLOW_GREEN)
+        self.sea_star = SeaStar(250, 250, 12, arcade.color.ORANGE_RED)
 
     def on_draw(self):
         arcade.start_render()
@@ -68,11 +67,11 @@ class MyGame(arcade.Window):
         draw_bubbles(52, 65)
         draw_bubbles(-120, 219)
         draw_bubbles(560, -90)
-        self.ball.draw()
+        self.sea_star.draw()
 
-        def on_mouse_motion(self, x, y, dx, dy):
-            self.ball.position_x = x
-            self.ball.position_y = y
+    def on_mouse_motion(self, x, y, dx, dy):
+        self.sea_star.position_x = x
+        self.sea_star.position_y = y
 
 
 def main():
