7fe2008243f0c2750ae211011991f3be141bbbda
spumarf <spumarf@gmail.com>
2023-03-16 16:27:18 +0100

    Cuarto commit y primer push de la sesión de LAB-6

--- lab08-sprites/lab08-sprites.py
+++ lab08-sprites/lab08-sprites.py
@@ -4,12 +4,12 @@ import random
 import arcade
 
 # --- Constants ---
-SPRITE_SCALING_ALIEN = 0.8
-SPRITE_SCALING_PLANET = 0.5
+SPRITE_SCALING_ALIEN = 1
+SPRITE_SCALING_PLANET = 0.08
 COIN_COUNT = 50
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
 
 
 class MyGame(arcade.Window):
@@ -42,10 +42,35 @@ class MyGame(arcade.Window):
         self.player_sprite.center_y = 50
         self.player_list.append(self.player_sprite)
 
+        for i in range(COIN_COUNT):
+            # All the sprites are from kenney.nl
+            planet = arcade.Sprite(":resources:planet.png", SPRITE_SCALING_PLANET)
+            planet.center_x = random.randrange(SCREEN_WIDTH)
+            planet.center_y = random.randrange(SCREEN_HEIGHT)
+            self.planet_list.append(planet)
+
     def on_draw(self):
         arcade.start_render()
         self.planet_list.draw()
         self.player_list.draw()
+
+        output = f"Score: {self.score}"
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 14)
+
+    def on_mouse_motion(self, x, y, dx, dy):
+        """ Handle Mouse Motion """
+        self.player_sprite.center_x = x
+        self.player_sprite.center_y = y
+
+    def update(self, delta_time):
+        """ Movement and game logic """
+        self.planet_list.update()
+        planet_hit_list = arcade.check_for_collision_with_list(self.player_sprite, self.planet_list)
+
+        for planet in planet_hit_list:
+            planet.remove_from_sprite_lists()
+            self.score += 1
+
 
 def main():
     """ Main method """
