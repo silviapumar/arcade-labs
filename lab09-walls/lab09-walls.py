ba298683611143d3c6b7b1cc6ee8f6ac863791c6
spumarf <spumarf@gmail.com>
2023-03-30 15:31:54 +0200

    Commit de la sesión LAB-9

--- lab09-walls/lab09-walls.py
+++ lab09-walls/lab09-walls.py
@@ -26,6 +26,8 @@ class MyGame(arcade.Window):
 
         self.physics_engine = None
 
+        self.physics_engine = None
+
     def setup(self):
         arcade.set_background_color(arcade.color.SPACE_CADET)
 
@@ -34,7 +36,6 @@ class MyGame(arcade.Window):
 
         self.score = 0
 
-        # Create the player
         self.player_sprite = arcade.Sprite(":resources:alien.png", SPRITE_SCALING_ALIEN)
         self.player_sprite.center_x = 50
         self.player_sprite.center_y = 64
@@ -50,11 +51,47 @@ class MyGame(arcade.Window):
         meteor.center_y = 200
         self.meteor_list.append(meteor)
 
+        for x in range(173, 650, 64):
+            meteor = arcade.Sprite(":resources:meteor.png", SPRITE_SCALING_METEOR)
+            meteor.center_x = x
+            meteor.center_y = 350
+            self.meteor_list.append(meteor)
+
+        coordinate_list = [[400, 500], [470, 500], [400, 570], [470, 570]]
+
+        for coordinate in coordinate_list:
+            meteor = arcade.Sprite(":resources:meteor.png", SPRITE_SCALING_METEOR)
+            meteor.center_x = coordinate[0]
+            meteor.center_y = coordinate[1]
+            self.meteor_list.append(meteor)
+
+        self.physics_engine = arcade.PhysicsEngineSimple(self.player_sprite, self.meteor_list)
+
     def on_draw(self):
         arcade.start_render()
 
         self.meteor_list.draw()
         self.player_list.draw()
+
+    def on_key_press(self, key, modifiers):
+        """Called whenever a key is pressed. """
+
+        if key == arcade.key.UP:
+            self.player_sprite.change_y = MOVEMENT_SPEED
+        elif key == arcade.key.DOWN:
+            self.player_sprite.change_y = -MOVEMENT_SPEED
+        elif key == arcade.key.LEFT:
+            self.player_sprite.change_x = -MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT:
+            self.player_sprite.change_x = MOVEMENT_SPEED
+
+    def on_key_release(self, key, modifiers):
+        """Called when the user releases a key. """
+
+        if key == arcade.key.UP or key == arcade.key.DOWN:
+            self.player_sprite.change_y = 0
+        elif key == arcade.key.LEFT or key == arcade.key.RIGHT:
+            self.player_sprite.change_x = 0
 
 
 def main():
